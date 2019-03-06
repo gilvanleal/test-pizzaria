768257041106f17eaa21ba949c3c1ace67228efb
Gilvan Leal <gilvan.sleal@gmail.com>
2019-03-05 22:30:09 -0300

    [IMP] pizzaria: Painel de administração; e migrations

--- pizzaria/admin.py
+++ pizzaria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pizza, Tamanho, Sabor, Personalizacao, Pedido
 
 # Register your models here.
+
+
+admin.site.register([Pizza, Tamanho, Sabor, Personalizacao, Pedido])
--- pizzaria_uds/urls.py
+++ pizzaria_uds/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
 
 urlpatterns = [
+    url(r'^', include('pizzaria.urls')),
     path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ]
